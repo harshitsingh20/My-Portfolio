02fc820f32d4d54e6e46cc6e102a6c32d21edc53
Harshit Singh <harshitsingh2035@gmail.com>
2023-11-20 11:03:39 +0530

    about section updated

--- components/about.tsx
+++ components/about.tsx
@@ -21,17 +21,19 @@ export default function About() {
       {/* <h2 className="text-3xl font-medium capitalize mb-8">About me</h2> */}
       <p className="mb-3">
         After graduating with a degree in{" "}
-        <span className="font-medium">Accounting</span>, I decided to pursue my
-        passion for programming. I enrolled in a coding bootcamp and learned{" "}
-        <span className="font-medium">full-stack web development</span>.{" "}
+        <span className="font-medium">B.tech</span><span className="font-medium">{" "}(Computer Science and Engineering){" "}</span>
+        specialization in {" "}<span className="font-medium">Cloud Technology and Information Security</span>
+        , I decided to pursue my
+        passion in <span className="font-medium">full-stack web development</span>.{" "}
+        
         <span className="italic">My favorite part of programming</span> is the
         problem-solving aspect. I <span className="underline">love</span> the
         feeling of finally figuring out a solution to a problem. My core stack
         is{" "}
         <span className="font-medium">
-          React, Next.js, Node.js, and MongoDB
+          React, Next.js, Node.js, MongoDB, MySQL, Python, AWS and Docker
         </span>
-        . I am also familiar with TypeScript and Prisma. I am always looking to
+        . I am also familiar with <span className="font-medium">TypeScript, PostgreSQL, Redis and Prisma.</span> I am always looking to
         learn new technologies. I am currently looking for a{" "}
         <span className="font-medium">full-time position</span> as a software
         developer.
@@ -42,7 +44,7 @@ export default function About() {
         video games, watching movies, and playing with my dog. I also enjoy{" "}
         <span className="font-medium">learning new things</span>. I am currently
         learning about{" "}
-        <span className="font-medium">history and philosophy</span>. I'm also
+        <span className="font-medium">Cryptocurrency and NFT</span>. I'm also
         learning how to play the guitar.
       </p>
     </motion.section>
