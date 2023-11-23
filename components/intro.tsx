67892b6f1fe25db561bba2fb26337e4038786175
Harshit Singh <harshitsingh2035@gmail.com>
2023-11-23 11:10:41 +0530

    Intro change & resume update & skilles updated

--- components/intro.tsx
+++ components/intro.tsx
@@ -32,7 +32,7 @@ export default function Intro() {
           >
             <Image
               src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?fit=crop&w=368&h=368&q=100"
-              alt="Ricardo portrait"
+              alt="Harshit Singh"
               width="192"
               height="192"
               quality="95"
@@ -91,7 +91,7 @@ export default function Intro() {
 
         <a
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
-          href="/CV.pdf"
+          href="/Harshit-CV.pdf"
           download
         >
           Download CV{" "}
@@ -100,7 +100,7 @@ export default function Intro() {
 
         <a
           className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://linkedin.com"
+          href="https://www.linkedin.com/in/harshit-singh-as/"
           target="_blank"
         >
           <BsLinkedin />
@@ -108,7 +108,7 @@ export default function Intro() {
 
         <a
           className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://github.com"
+          href="https://github.com/harshitsingh20"
           target="_blank"
         >
           <FaGithubSquare />
